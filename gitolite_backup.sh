9cd584425ad6d3bb18516d587d4ec409f2fd71bd
Kurucz István <kurucz.istvan@gmail.com>
2016-07-05 01:04:11 +0200

    delete old backup files

--- gitolite_backup.sh
+++ gitolite_backup.sh
@@ -29,6 +29,26 @@ function script_exit_common {
 function script_exit_ok {
   # exit function: success operation
   script_exit_common ok "$*"
+  if [[ BACKUP_FILE_COUNTER_LIMIT -gt 0 ]]
+  then
+    # delete old backup files
+    export BACKUP_FILES=( $(ls -1cr $BACKUP_DIR) )  # get file list, reverse sort by ctime
+    export BACKUP_FILES_NUMBER=${#BACKUP_FILES[@]}  # number of files
+    i="0"
+    while [ $i -lt $[BACKUP_FILES_NUMBER-BACKUP_FILE_COUNTER_LIMIT] ]
+    do
+      rm -f $BACKUP_DIR/${BACKUP_FILES[$i]}
+      if [[ ! -e $BACKUP_DIR/${BACKUP_FILES[$i]} ]]
+      then
+        echo "backup file deleted: "${BACKUP_FILES[$i]}
+        logger "gitolite backup - success: backup file deleted: "${BACKUP_FILES[$i]}
+      else
+        echo "backup file not deleted: "${BACKUP_FILES[$i]}
+        logger "gitolite backup - error: backup file not deleted: "${BACKUP_FILES[$i]}
+      fi
+      i=$[$i+1]
+    done
+  fi
   echo $1
   logger "gitolite backup - success:" $1
   exit 0
@@ -51,16 +71,18 @@ export GITOLITE_REPO_DIR="/srv/gitolite/repositories"  # source gitolite directo
 export TEMP_DIR="/tmp/repo_backup"  # temporary directory
 export BACKUP_FILENAME="gitolite-$DATE.tar.xz"  # backup filename
 export GIT_PUSH_DISABLED_MESSAGE="please wait"  # "git push" disabled message
+export BACKUP_FILE_COUNTER_LIMIT="0"  # maximum count of backup files, 0: not delete old files
 export COMPRESS_PARAMS="XZ_OPT=-9"  # compression type and level
 export GPG_PARAMS="--cipher-algo AES256" # gpg command-line parameters
 export TAR_PARAMS="-Jcvf"  # tar command-line parameters
 # export GPG_PASSWORD=""  # use plain-text password for encrypted backup
-export GPG_PASSWORD=`cat archive_password.txt`  # use password file for encrypted backup
+# export GPG_PASSWORD=`cat archive_password.txt`  # use password file for encrypted backup
 
 
 ## main program
 echo $GIT_PUSH_DISABLED_MESSAGE | $GITOLITE writable @all off  # disable "git push" command
 find $GITOLITE_REPO_DIR -name '*.git' -type d -exec bash -c 'clean_repos "$0"' {} \;
+mkdir -p $BACKUP_DIR
 if [[ -n $GPG_PASSWORD ]]
 then
   # encrypt archive
