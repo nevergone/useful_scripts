82a066d82f3e89e203e2712446d9043e3cd310f4
Kurucz István <kurucz.istvan@gmail.com>
2016-07-06 14:43:59 +0200

    more error handling

--- gitolite_backup.sh
+++ gitolite_backup.sh
@@ -31,19 +31,16 @@ function script_exit_common {
 function script_exit_ok {
   # exit function: success operation
   script_exit_common ok "$*"
-  if [[ BACKUP_FILE_COUNTER_LIMIT -gt 0 ]]
-  then
+  if [[ BACKUP_FILE_COUNTER_LIMIT -gt 0 ]]; then
     # delete old backup files
     OIFS="$IFS"  # backup IFS
     IFS=$'\n'
     export BACKUP_FILES=( $(ls -1crp "$BACKUP_DIR" | grep -v / ) )  # get file list, reverse sort by ctime
     export BACKUP_FILES_NUMBER=${#BACKUP_FILES[@]}  # number of files
     i="0"
-    while [ $i -lt $((BACKUP_FILES_NUMBER-BACKUP_FILE_COUNTER_LIMIT)) ]
-    do
+    while [ $i -lt $((BACKUP_FILES_NUMBER-BACKUP_FILE_COUNTER_LIMIT)) ]; do
       rm -f "$BACKUP_DIR/${BACKUP_FILES[$i]}"
-      if [[ ! -e "$BACKUP_DIR/${BACKUP_FILES[$i]}" ]]
-      then
+      if [[ ! -e "$BACKUP_DIR/${BACKUP_FILES[$i]}" ]]; then
         echo "backup file deleted: ${BACKUP_FILES[$i]}"
         logger "gitolite backup - success: backup file deleted: ${BACKUP_FILES[$i]}"
       else
@@ -53,7 +50,7 @@ function script_exit_ok {
       fi
       i=$((i+1))
     done
-  IFS="$OIFS"  # restore original IFS
+    IFS="$OIFS"  # restore original IFS
   fi
   echo $1
   logger "gitolite backup - success: $1"
@@ -86,30 +83,32 @@ export TAR_PARAMS="-Jcvf"  # tar command-line parameters
 
 
 ## main program
+if [ ! -d "$GITOLITE_REPO_DIR" -o ! -r "$GITOLITE_REPO_DIR" ]; then
+  script_exit_error "source directory not exist or not readable"
+fi
 echo "$GIT_PUSH_DISABLED_MESSAGE" | $GITOLITE writable @all off  # disable "git push" command
+if [ $? -ne 0 ]; then
+  script_exit_error "gitolite error"
+fi
 find "$GITOLITE_REPO_DIR" -name '*.git' -type d -exec bash -c 'clean_repos "$0"' {} \;
 mkdir -p "$BACKUP_DIR"
-if [[ -n $GPG_PASSWORD ]]
-then
+if [[ -n $GPG_PASSWORD ]]; then
   # encrypt archive
   mkdir -p "$TEMP_DIR"
   chmod 700 "$TEMP_DIR"
   export $COMPRESS_PARAMS; tar $TAR_PARAMS "$TEMP_DIR/$BACKUP_FILENAME" "$GITOLITE_REPO_DIR"
-  if [[ ! -e $TEMP_DIR/$BACKUP_FILENAME ]]
-  then
+  if [[ ! -e $TEMP_DIR/$BACKUP_FILENAME ]]; then
     script_exit_error "backup file not exist: $BACKUP_FILENAME"
   fi
   echo "$GPG_PASSWORD" | $GPG $GPG_PARAMS --passphrase-fd 0 -c "$TEMP_DIR/$BACKUP_FILENAME"
-  if [[ ! -e "$TEMP_DIR/$BACKUP_FILENAME.gpg" ]]
-  then
+  if [[ ! -e "$TEMP_DIR/$BACKUP_FILENAME.gpg" ]]; then
     script_exit_error "gpg file not exist: $BACKUP_FILENAME.gpg"
   fi
   mv "$TEMP_DIR/$BACKUP_FILENAME.gpg" "$BACKUP_DIR/"
 else
   # non-encrypt archive
   export $COMPRESS_PARAMS; tar $TAR_PARAMS "$BACKUP_DIR/$BACKUP_FILENAME" "$GITOLITE_REPO_DIR"
-  if [[ ! -e "$BACKUP_DIR/$BACKUP_FILENAME" ]]
-  then
+  if [[ ! -e "$BACKUP_DIR/$BACKUP_FILENAME" ]]; then
     script_exit_error "backup file not exist: $BACKUP_FILENAME"
   fi
 fi
